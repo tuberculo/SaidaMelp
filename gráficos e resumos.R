b9ad7b69cdf14f1f9af670d5a045cfed26024793
tuberculo <maltacastro@gmail.com>
2020-02-13 20:57:35 +0100

    Gráfico com todas as fontes.

--- gráficos e resumos.R
+++ gráficos e resumos.R
@@ -29,19 +29,30 @@ escolhidos <- c(Caso_min, 57, 54, 56)
 
 # ParaGraf <- GerAnualporTipoTerm
 # ParaGraf$Caso <- factor(GerAnualporTipoTerm$Caso, levels = c(Caso_min, 57, 54, 56), labels = c("Referência","IS25","IS22","IS21"))
+# Soma das gerações de UHE e UTE por ano:
+GerAnualAgrup <- group_by(BalancoTudo, Caso, Ano, Patamar, Subsistema) %>% filter(Energia == "Media") %>% summarise(Hid = mean(GeraHidrEnerg), Ter = mean(GeraTerEnerg)) %>% ungroup() %>% group_by(Caso, Ano) %>% summarise(HidrTot = sum(Hid), TermTot = sum(Ter))
 
-GerAnualporTipoTerm <- mutate(GerAnualporTipoTerm, NomeCaso = factor(Caso, levels = escolhidos, labels = c("Referência","IS25","IS22","IS21")))
+GerAnualporTipo <- bind_rows(GerAnualporTipoTerm, rename(mutate(GerAnualAgrup, Tipo = "Hidro", Energia = "Media"), Gera = HidrTot))
+GerAnualporTipo <- mutate(GerAnualporTipo, NomeCaso = factor(Caso, levels = escolhidos, labels = c("Referência","IS25","IS22","IS21")))
+GerAnualporTipo$Tipo <- factor(GerAnualporTipo$Tipo, levels = c("UFV", "Eol", "CSP", "Hidro", "Bagaco", "OutrosExistentes", "Gas", "Carvao", "Nuclear"))
 
 # Gráfico de área
-ggplot(filter(GerAnualporTipoTerm, Caso %in% escolhidos, Tipo %in% c("Carvao", "Gas", "Nuclear", "OutrosExistentes"), 
+ggplot(filter(GerAnualporTipo, Caso %in% escolhidos, Tipo %in% c("Carvao", "Gas", "Nuclear", "OutrosExistentes"), 
   Energia == "Media")) + geom_area(aes(x = Ano, y = Gera, fill = Tipo)) + facet_grid(~NomeCaso) + 
   scale_fill_brewer(palette = "BrBG") + ggtitle("Geração das usinas despacháveis") + xlab("") +
   ylab("Geração (MWano)") + labs(fill = "Fonte") + theme(axis.text.x = element_text(angle = 50, hjust = 1)) + 
   theme(plot.title = element_text(hjust = 0.5)) + theme(text = element_text(size = 16)) + 
   geom_vline(aes(xintercept = as.numeric(2022)), linetype = 4, colour = "black")
 #Gráfico de coluna
-ggplot(filter(GerAnualporTipoTerm, Caso %in% escolhidos, Tipo %in% c("Carvao", "Gas", "Nuclear", "OutrosExistentes"), 
+ggplot(filter(GerAnualporTipo, Caso %in% escolhidos, Tipo %in% c("Carvao", "Gas", "Nuclear", "OutrosExistentes"), 
   Energia == "Media")) + geom_col(aes(x = Ano, y = Gera, fill = Tipo)) + facet_grid(~NomeCaso) + 
+  scale_fill_brewer(palette = "BrBG") + ggtitle("Geração das usinas despacháveis") + xlab("") +
+  ylab("Geração (MWano)") + labs(fill = "Fonte") + theme(axis.text.x = element_text(angle = 50, hjust = 1)) + 
+  theme(plot.title = element_text(hjust = 0.5)) + theme(text = element_text(size = 16))
+
+#Gráfico de todas as fontes
+ggplot(filter(GerAnualporTipo, Caso %in% escolhidos, 
+              Energia == "Media")) + geom_col(aes(x = Ano, y = Gera, fill = Tipo)) + facet_grid(~NomeCaso) + 
   scale_fill_brewer(palette = "BrBG") + ggtitle("Geração das usinas despacháveis") + xlab("") +
   ylab("Geração (MWano)") + labs(fill = "Fonte") + theme(axis.text.x = element_text(angle = 50, hjust = 1)) + 
   theme(plot.title = element_text(hjust = 0.5)) + theme(text = element_text(size = 16))
@@ -49,5 +60,3 @@ ggplot(filter(GerAnualporTipoTerm, Caso %in% escolhidos, Tipo %in% c("Carvao", "
 # ggplot(filter(GerAnualporTipoTerm, Tipo %in% c("Carvao", "Gas", "Nuclear", "OutrosExistentes"), 
 #       Energia == "Media")) + geom_col(aes(x = Ano, y = Gera, fill = Tipo)) + facet_wrap(vars(Caso)) + scale_fill_brewer(palette = "BrBG")
 
-# Soma das gerações de UHE e UTE por ano:
-GerAnualporTipo <- group_by(BalancoTudo, Caso, Ano, Patamar, Subsistema) %>% filter(Energia == "Media") %>% summarise(Hid = mean(GeraHidrEnerg), Ter = mean(GeraTerEnerg)) %>% ungroup() %>% group_by(Caso, Ano) %>% summarise(HidrTot = sum(Hid), TermTot = sum(Ter))
