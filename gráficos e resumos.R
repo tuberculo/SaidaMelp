61d403e4706d7dccfdfbbc2d1a7776a9c85402bf
tuberculo <maltacastro@gmail.com>
2020-02-14 11:54:39 +0100

    Novos gráficos.

--- gráficos e resumos.R
+++ gráficos e resumos.R
@@ -1,3 +1,4 @@
+library(tidyverse)
 
 PatamarTudo <- readRDS("PatamarTudo.rds")
 TermTudo <- readRDS("TermTudo.rds")
@@ -34,7 +35,7 @@ GerAnualAgrup <- group_by(BalancoTudo, Caso, Ano, Patamar, Subsistema) %>% filte
 
 GerAnualporTipo <- bind_rows(GerAnualporTipoTerm, rename(mutate(GerAnualAgrup, Tipo = "Hidro", Energia = "Media"), Gera = HidrTot))
 GerAnualporTipo <- mutate(GerAnualporTipo, NomeCaso = factor(Caso, levels = escolhidos, labels = c("Referência","IS25","IS22","IS21")))
-GerAnualporTipo$Tipo <- factor(GerAnualporTipo$Tipo, levels = c("UFV", "Eol", "CSP", "Hidro", "Bagaco", "OutrosExistentes", "Gas", "Carvao", "Nuclear"))
+GerAnualporTipo$Tipo <- factor(GerAnualporTipo$Tipo, levels = c("UFV", "Eol", "CSP", "Hidro", "Bagaco", "Gas", "Carvao", "Nuclear", "OutrosExistentes"))
 
 # Gráfico de área
 ggplot(filter(GerAnualporTipo, Caso %in% escolhidos, Tipo %in% c("Carvao", "Gas", "Nuclear", "OutrosExistentes"), 
@@ -50,10 +51,24 @@ ggplot(filter(GerAnualporTipo, Caso %in% escolhidos, Tipo %in% c("Carvao", "Gas"
   ylab("Geração (MWano)") + labs(fill = "Fonte") + theme(axis.text.x = element_text(angle = 50, hjust = 1)) + 
   theme(plot.title = element_text(hjust = 0.5)) + theme(text = element_text(size = 16))
 
+  #Gráfico só das usinas já existentes
+ggplot(filter(GerAnualporTipo, Caso %in% escolhidos, Tipo %in% c("OutrosExistentes"), 
+              Energia == "Media")) + geom_col(aes(x = Ano, y = Gera, fill = Tipo)) + facet_grid(~NomeCaso) + 
+  scale_fill_brewer(palette = "BrBG") + ggtitle("Geração de todas as usinas") + xlab("") +
+  ylab("Geração (MWano)") + labs(fill = "Fonte") + theme(axis.text.x = element_text(angle = 50, hjust = 1)) + 
+  theme(plot.title = element_text(hjust = 0.5)) + theme(text = element_text(size = 16))
+
 #Gráfico de todas as fontes
 ggplot(filter(GerAnualporTipo, Caso %in% escolhidos, 
               Energia == "Media")) + geom_col(aes(x = Ano, y = Gera, fill = Tipo)) + facet_grid(~NomeCaso) + 
-  scale_fill_brewer(palette = "BrBG") + ggtitle("Geração das usinas despacháveis") + xlab("") +
+  scale_fill_brewer(palette = "BrBG") + ggtitle("Geração das usinas existentes") + xlab("") +
+  ylab("Geração (MWano)") + labs(fill = "Fonte") + theme(axis.text.x = element_text(angle = 50, hjust = 1)) + 
+  theme(plot.title = element_text(hjust = 0.5)) + theme(text = element_text(size = 16))
+
+#Gráfico de despacháveis separadamente
+ggplot(filter(GerAnualporTipo, Caso %in% escolhidos, Tipo %in% c("Carvao", "Gas", "Nuclear", "OutrosExistentes"),
+              Energia == "Media")) + geom_col(aes(x = NomeCaso, y = Gera)) + facet_grid(Tipo ~ Ano) + 
+  scale_fill_brewer(palette = "BrBG") + ggtitle("Geração de despacháveis") + xlab("") +
   ylab("Geração (MWano)") + labs(fill = "Fonte") + theme(axis.text.x = element_text(angle = 50, hjust = 1)) + 
   theme(plot.title = element_text(hjust = 0.5)) + theme(text = element_text(size = 16))
 
