1e66fdbda8d4bb4c6a62a58ddc731ccc21e4d6db
tuberculo <maltacastro@gmail.com>
2020-02-21 19:45:27 +0100

    Novo gráfico

--- gráficos e resumos.R
+++ gráficos e resumos.R
@@ -5,17 +5,26 @@ TermTudo <- readRDS("TermTudo.rds")
 BalancoTudo <- readRDS("BalancoTudo.rds")
 
 
-TermTudo <- mutate(TermTudo, Tipo = case_when(grepl("EOL", NOME) ~ "Eol", 
-                                              grepl("GAS", NOME) ~ "Gas",
-                                              grepl("CARV", NOME) ~ "Carvao",
+TermTudo <- mutate(TermTudo, Tipo = case_when(grepl("EOL", NOME) ~ "Eólica", 
+                                              grepl("GAS", NOME) ~ "Gás",
+                                              grepl("CARV", NOME) ~ "Carvão",
                                               grepl("SOL", NOME) ~ "UFV",
                                               grepl("CSP", NOME) ~ "CSP",
-                                              grepl("BAG", NOME) ~ "Bagaco",
+                                              grepl("BAG", NOME) ~ "Bagaço",
                                               grepl("NUC", NOME) ~ "Nuclear",
                                               grepl("ANGRA", NOME) ~ "Nuclear",
                                               TRUE ~ "OutrosExistentes"))
 
-GerAnualporTipoTerm <- group_by(TermTudo, Caso, Energia, Ano, Tipo) %>% summarise(Gera = sum(GeraEner) / 12)
+TermTudo <- mutate(TermTudo, NovaOuExistente = case_when(grepl("EOL", NOME) ~ "Nova", 
+                                               grepl("GAS", NOME) ~ "Nova",
+                                               grepl("CARV", NOME) ~ "Nova",
+                                               grepl("SOL", NOME) ~ "Nova",
+                                               grepl("CSP", NOME) ~ "Nova",
+                                               grepl("BAG", NOME) ~ "Nova",
+                                               grepl("NUC", NOME) ~ "Nova",
+                                               grepl("ANGRA 3", NOME) ~ "Nova",
+                                               TRUE ~ "Existente"))
+GerAnualporTipoTerm <- group_by(TermTudo, Caso, Energia, Ano, Tipo, NovaOuExistente) %>% summarise(Gera = sum(GeraEner) / 12)
 
 # Plot --------------------------------------------------------------------
 # Define os casos:
@@ -35,17 +44,17 @@ GerAnualAgrup <- group_by(BalancoTudo, Caso, Ano, Patamar, Subsistema) %>% filte
 
 GerAnualporTipo <- bind_rows(GerAnualporTipoTerm, rename(mutate(GerAnualAgrup, Tipo = "Hidro", Energia = "Media"), Gera = HidrTot))
 GerAnualporTipo <- mutate(GerAnualporTipo, NomeCaso = factor(Caso, levels = escolhidos, labels = c("Referência","IS25","IS22","IS21")))
-GerAnualporTipo$Tipo <- factor(GerAnualporTipo$Tipo, levels = c("UFV", "Eol", "CSP", "Hidro", "Bagaco", "Gas", "Carvao", "Nuclear", "OutrosExistentes"))
+GerAnualporTipo$Tipo <- factor(GerAnualporTipo$Tipo, levels = c("UFV", "Eol", "CSP", "Hidro", "Bagaço", "Gás", "Carvão", "Nuclear", "OutrosExistentes"))
 
 # Gráfico de área
-ggplot(filter(GerAnualporTipo, Caso %in% escolhidos, Tipo %in% c("Carvao", "Gas", "Nuclear", "OutrosExistentes"), 
+ggplot(filter(GerAnualporTipo, Caso %in% escolhidos, Tipo %in% c("Carvão", "Gás", "Nuclear", "OutrosExistentes"), 
   Energia == "Media")) + geom_area(aes(x = Ano, y = Gera, fill = Tipo)) + facet_grid(~NomeCaso) + 
   scale_fill_brewer(palette = "BrBG") + ggtitle("Geração das usinas despacháveis") + xlab("") +
   ylab("Geração (MWano)") + labs(fill = "Fonte") + theme(axis.text.x = element_text(angle = 50, hjust = 1)) + 
   theme(plot.title = element_text(hjust = 0.5)) + theme(text = element_text(size = 16)) + 
   geom_vline(aes(xintercept = as.numeric(2022)), linetype = 4, colour = "black")
 #Gráfico de coluna
-ggplot(filter(GerAnualporTipo, Caso %in% escolhidos, Tipo %in% c("Carvao", "Gas", "Nuclear", "OutrosExistentes"), 
+ggplot(filter(GerAnualporTipo, Caso %in% escolhidos, Tipo %in% c("Carvão", "Gás", "Nuclear", "OutrosExistentes"), 
   Energia == "Media")) + geom_col(aes(x = Ano, y = Gera, fill = Tipo)) + facet_grid(~NomeCaso) + 
   scale_fill_brewer(palette = "BrBG") + ggtitle("Geração das usinas despacháveis") + xlab("") +
   ylab("Geração (MWano)") + labs(fill = "Fonte") + theme(axis.text.x = element_text(angle = 50, hjust = 1)) + 
@@ -66,11 +75,18 @@ ggplot(filter(GerAnualporTipo, Caso %in% escolhidos,
   theme(plot.title = element_text(hjust = 0.5)) + theme(text = element_text(size = 16))
 
 #Gráfico de despacháveis separadamente
-ggplot(filter(GerAnualporTipo, Caso %in% escolhidos, Tipo %in% c("Carvao", "Gas", "Nuclear", "OutrosExistentes"),
+ggplot(filter(GerAnualporTipo, Caso %in% escolhidos, Tipo %in% c("Carvão", "Gás", "Nuclear", "OutrosExistentes"),
               Energia == "Media")) + geom_col(aes(x = NomeCaso, y = Gera)) + facet_grid(Tipo ~ Ano) + 
   scale_fill_brewer(palette = "BrBG") + ggtitle("Geração de despacháveis") + xlab("") +
   ylab("Geração (MWano)") + labs(fill = "Fonte") + theme(axis.text.x = element_text(angle = 50, hjust = 1)) + 
-  theme(plot.title = element_text(hjust = 0.5)) + theme(text = element_text(size = 16))
+  theme(plot.title = element_text(hjust = 0.5)) + theme(text = element_text(size = 12))
+
+#Gráfico de despacháveis "nova ou existente" até 2030
+ggplot(filter(GerAnualporTipo, Caso %in% escolhidos, Tipo %in% c("Carvão", "Gás", "Nuclear", "OutrosExistentes"),
+              Energia == "Media", Ano <= 2030, Ano >= 2020)) + geom_col(aes(x = NomeCaso, y = Gera)) + 
+  facet_grid(NovaOuExistente ~ Ano) +   scale_fill_brewer(palette = "BrBG") + ggtitle("Geração de despacháveis") + xlab("") +
+  ylab("Geração (MWano)") + labs(fill = "Fonte") + theme(plot.title = element_text(hjust = 0.5)) + 
+  theme(text = element_text(size = 16)) + theme(axis.text.x = element_text(angle = 50, hjust = 1, size = 12))
 
 # ggplot(filter(GerAnualporTipoTerm, Tipo %in% c("Carvao", "Gas", "Nuclear", "OutrosExistentes"), 
 #       Energia == "Media")) + geom_col(aes(x = Ano, y = Gera, fill = Tipo)) + facet_wrap(vars(Caso)) + scale_fill_brewer(palette = "BrBG")
